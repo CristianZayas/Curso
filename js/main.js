6a632cc7b766575a41f9987715feccbb9f8586c4
Cristian Zayas <josecristianzayas@gmail.com>
2021-02-14 22:26:34 -0600

    Mejoras del html

--- js/main.js
+++ js/main.js
@@ -9,10 +9,14 @@
 
        myModal.addEventListener('show.bs.modal', () => console.log('show.bs.modal') )
        myModal.addEventListener('shown.bs.modal', () => {
-        btnModal.className = 'container btn  btn-danger'
+        btnModal.className = 'container btn  btn-danger disabled'
+      
         btnModal.textContent = "Estas en la Modal"
          console.log('shown.bs.modal')} )
-       myModal.addEventListener('hide.bs.modal', () => console.log('hide.bs.modal') )
+       myModal.addEventListener('hide.bs.modal', () => {
+        btnModal.className = 'container btn btn-warning'
+        btnModal.textContent = "Mostrar la frase."
+       } )
        myModal.addEventListener('hidden.bs.modal', () => console.log('hidden.bs.modal') )
 
        
